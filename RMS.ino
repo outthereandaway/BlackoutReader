530ead19c85521f682c4b75f7efb951c7d32ca6f
outthereandaway <40517411+outthereandaway@users.noreply.github.com>
2018-07-01 18:14:16 +0200

    Add files via upload

--- RMS.ino
+++ RMS.ino
@@ -19,11 +19,11 @@ const char *appEui = "70B3D57ED0010011";
 const char *appKey = "BBABB7D4579913C4EC8407A8E3024FCB";
 
 // parameters for sending messages over LoRa
-const unsigned int intervMess = 60; // interval for sending "
-unsigned long lastMess = 0;
+const unsigned int intervMess = 5; // interval for sending
+unsigned long lastMess = 0; // variable to store timestamp of last message sent
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
-byte payload[3];
+byte payload[3]; // array to store message
 
 
 // identifier for the line and phase where our sensor is connected to
@@ -37,11 +37,12 @@ const unsigned long sampleTime = 100000UL;
 const unsigned long numSamples = 250UL;
 const unsigned long sampleInterval = sampleTime/numSamples;
 
-float voltEff = 0; // actual measured voltage value
-byte i=0; 
+float voltEff = 0; // actual measured voltage value during normal voltage state
+byte i = 0; // variable to count, assign and store values for storVal array 
 float storVal[10]; // stored previous voltage values
-bool pwlOn = false; // power on/off
-float voltDrop=0; 
+bool pwlOn = true; // power on/off
+float voltDrop = 0; // actual measured voltage during voltage drop state
+float avVoltDrop = 0; // average of previous voltage values during voltage drop situation
 
 
 
@@ -68,10 +69,18 @@ void setup() {
   debugSerial.println("-- JOIN");
   ttn.join(appEui, appKey);
 
+  // creating and sending of first message
+  payload[0] = lineIdent;
+  payload[1] = phaseIdent;
+  payload[2] = pwlOn;
+  debugSerial.println("-- SEND");
+  ttn.sendBytes(payload, sizeof(payload));
+
   // reference for analog input. DEFAULT is 5V internal voltage. Problem --> not constant
   analogReference(DEFAULT);
 
-  for (byte k=0;k=9;k++){
+  // filling the storVal array with measurement values
+  for (byte k = 0; k <= 9; k++) {
     voltEff = voltSense(input, sampleInterval, numSamples); // taking measurements
     storVal[k] = voltEff;
     delay(500);
@@ -88,77 +97,125 @@ void setup() {
 void loop() {
 // put your main code here, to run repeatedly:
  
-  float AvStorVal=0;
+  float avStorVal = 0; // setting the average of all values stored in storVal to 0
 
-  for (byte j=0;j=9;j++){
-    AvStorVal=AvStorVal+storVal[j];
-   }
-  Serial.println(AvStorVal);
-//    
-//  voltEff = voltSense(input, sampleInterval, numSamples); // taking measurements
-//  storVal[i] = voltEff;
-//  Serial.println(voltEff); // printing measurements on serial monitor
-//
-//
-//  if (((AvStorVal/10)*0.01) > voltEff) {
-//
-//  byte DropCount=0;
-//    
-//    for (byte p=0;p=2;p++){
-//      voltDrop = voltSense(input, sampleInterval, numSamples); // taking measurements
-//
-//      
-//      if (((AvStorVal/10)*0.01) > voltDrop) {
-//        DropCount=DropCount+1;
-//      }
-//      
-//    }
-//
-//    if (DropCount=3){
-//      pwlOn = false;
-//      payload[0] = lineIdent;
-//      payload[1] = phaseIdent;
-//      payload[2] = pwlOn;
-//      debugSerial.println("-- SEND");
-//      ttn.sendBytes(payload, sizeof(payload));
-//    }
-//    
-//  }
-//
-//
-//  if ((storVal[i] / 0.05) < voltEff) {
-//
-//    pwlOn = true;
-//    payload[0] = lineIdent;
-//    payload[1] = phaseIdent;
-//    payload[2] = pwlOn;
-//    debugSerial.println("-- SEND");
-//    ttn.sendBytes(payload, sizeof(payload));
-//    
-//  }
-//
-//  
-//  //storVal[i] = voltEff;
-//  
-//  delay(500);
-//
-//  
-//  if (millis() >= lastMess + (intervMess * 1000)) {
-//
-//    payload[0] = lineIdent;
-//    payload[1] = phaseIdent;
-//    payload[2] = pwlOn;
-//    debugSerial.println("-- SEND");
-//    ttn.sendBytes(payload, sizeof(payload));
-//    lastMess = millis();
-//    
-//  }
-//
-//  i++;
-//
-//  if (i>9){
-//    i=0;
-//  }
+  // calculating new average of all values stored in storVal
+  for (byte j = 0; j <= 9; j++) {
+    
+    avStorVal = avStorVal + storVal[j];
+    
+  }
+
+  avStorVal = avStorVal / 10;
+
+  // taking measurements and printing them on serial monitor
+  voltEff = voltSense(input, sampleInterval, numSamples); 
+  Serial.println(voltEff); 
+
+  // comparing new measurement against average of measurements in storVal
+  if ((avStorVal * 0.05) > voltEff) {
+
+    float storVoltDrop[3];  // new array for measurements taken after voltage drop
+    byte dropCount = 0; // counter for measurements after voltage drop
+
+    // taking 3 measurements 
+    for (byte p = 0; p <= 2; p++) {
+      
+      voltDrop = voltSense(input, sampleInterval, numSamples); // taking measurements
+
+      Serial.println(voltDrop);
+      delay(500);
+
+      // if taken measurements are still on very low voltage level (voltage drop), put them into storVoltDrop array
+      if ((avStorVal * 0.05) > voltDrop) {
+
+        dropCount = dropCount + 1;
+        storVoltDrop[p] = voltDrop;
+        
+      }      
+      
+    }
+
+    // calculation of average of values in storVoltDrop array
+    avVoltDrop = 0;
+    
+    for (byte g = 0; g <= 2; g++) {
+
+      avVoltDrop = avVoltDrop + storVoltDrop[g];
+        
+    }
+
+    avVoltDrop = avVoltDrop / 3;
+
+    // if 3 measurements fullfill the voltage drop condition, a alarm message is sent through LoRa and state of the system is changed to "false"
+    if (dropCount == 3 && pwlOn == true) {
+      
+      pwlOn = false;
+      payload[0] = lineIdent;
+      payload[1] = phaseIdent;
+      payload[2] = pwlOn;
+      debugSerial.println("-- SEND");
+      ttn.sendBytes(payload, sizeof(payload));
+      lastMess = millis();    
+
+    }
+
+    
+    // if voltage measured is increasing again, message is set through LoRa and state of system is changed to "true"
+    if ((voltDrop * 0.05) > avVoltDrop && pwlOn == false) {
+
+      pwlOn = true;
+      payload[0] = lineIdent;
+      payload[1] = phaseIdent;
+      payload[2] = pwlOn;
+      debugSerial.println("-- SEND");
+      ttn.sendBytes(payload, sizeof(payload));
+      lastMess = millis();
+    
+    }
+  
+  }
+
+  // if voltage measured is increasing again, message is set through LoRa and state of system is changed to "true"
+  if ((voltEff * 0.05) > avVoltDrop && pwlOn == false) {
+
+    pwlOn = true;
+    payload[0] = lineIdent;
+    payload[1] = phaseIdent;
+    payload[2] = pwlOn;
+    debugSerial.println("-- SEND");
+    ttn.sendBytes(payload, sizeof(payload));
+    lastMess = millis();
+    
+  }
+
+  // if no message has been sent during the timed defined in intervMess, a message is being sent with the current state of the system ("true"/"false")
+  if (millis() >= lastMess + (intervMess * 1000)) {
+
+    payload[0] = lineIdent;
+    payload[1] = phaseIdent;
+    payload[2] = pwlOn;
+    debugSerial.println("-- SEND");
+    ttn.sendBytes(payload, sizeof(payload));
+    lastMess = millis();
+    
+  }
+
+  // storage of values in storVal array during normal measurement cycle (no voltge drop) and incrementing of counter (or reseting in case of reaching the limit)
+  if (pwlOn == true) {
+    
+      storVal[i] = voltEff;
+      i++;
+
+        if (i > 9) {
+          
+          i = 0;
+          
+        }
+          
+    }
+
+    delay(500);
 
 }
 
--- voltSense.h
+++ voltSense.h
@@ -1,10 +1,13 @@
 float voltSense(const unsigned int input, unsigned long sampleInterval, unsigned long numSamples) {
 
-  const float vpc = 4.8828125;  
-  unsigned long sampleAccAV = 0;
-  unsigned int count = 0;
-  unsigned long prevMicros = micros() - sampleInterval;
+/* function the sense the voltage of ZMCT103C 5A Range Single Phase AC Active Output Current Transformer Module */
 
+  const float vpc = 4.8828125; // increase/decrease of mV per step on Arduino analogue input ((5 / 1024) * 1000)
+  unsigned long sampleAccAV = 0; // average of accumulated samples
+  unsigned int count = 0; // counter for accumulations
+  unsigned long prevMicros = micros() - sampleInterval; // storage for microseconds of last sample taken 
+
+  // calculation of average (arithmetic mean) of samples over sample interval. Since we're dealing with a somewhat periodic signal, the average defines the 0-value of the signal.
   while (count < numSamples) {
     
     if (micros() - prevMicros >= sampleInterval) {
@@ -16,9 +19,10 @@ float voltSense(const unsigned int input, unsigned long sampleInterval, unsigned
     }
 
   }
-
-  float sampleAV = (float)sampleAccAV / (float)numSamples;
   
+  float sampleAV = (float)sampleAccAV / (float)numSamples;
+
+  // calculation of rms value over sample interval. Every sample taken is corrected by the before calculated average (0-value).
   unsigned long sampleAccRMS = 0;
   count = 0;
   prevMicros = micros() - sampleInterval;
@@ -36,8 +40,8 @@ float voltSense(const unsigned int input, unsigned long sampleInterval, unsigned
   }
 
   float sampleRMS = sqrt((float)sampleAccRMS / (float)numSamples);
-  //float sampleAV = (float)sampleAccAV / (float)numSamples;
-  float voltEff = sampleRMS * vpc;
+  
+  float voltEff = sampleRMS * vpc; // conversion of measured value (0 - 1023) to mV value
 
 return (voltEff);
   
